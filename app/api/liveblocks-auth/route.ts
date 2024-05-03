53e431081c911c54cbcce4931f581fcfa2a2cc2f
Siddharth263 <priyanshulal2213@gmail.com>
2024-05-03 17:11:49 +0530

    done

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -7,8 +7,7 @@ import { api } from "@/convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret:
-    "sk_dev_Hgit6FiyjzDKcYFtFJM-PxJaTjFEhansejF2Wgf6bfmOs3p2SbA_QWov7jmdyV-m",
+  secret: process.env.LIVEBLOCK_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
